1a8dae572270d00f44fc759750e0a2954b0db017
shwetha <shwethac841@gmail.com>
2026-07-23 18:00:27 +0530

    added login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,52 @@
+import "../styles/Login.css";
+import { useState } from "react";
+
 function Login() {
-    return <h1>Login</h1>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(email);
+        console.log(password);
+    };
+
+    return (
+        <div className="login-container">
+
+            <form className="login-form" onSubmit={handleSubmit}>
+
+                <h1>Welcome Back 👋</h1>
+
+                <p>Login to continue shopping.</p>
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit">
+                    Login
+                </button>
+
+                <p className="signup-text">
+                    Don't have an account? <span>Sign Up</span>
+                </p>
+
+            </form>
+
+        </div>
+    );
 }
 
 export default Login;
