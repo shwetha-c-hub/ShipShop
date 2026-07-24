33c95d6257d7368951779619ef3ab4452fc89c5a
shwetha <shwethac841@gmail.com>
2026-07-24 10:27:53 +0530

    fix navbar import case

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import ProductDetails from "./pages/ProductDetails";
 import Home from "./pages/Home";
